32ca196c9aa6b7046901327f1981f0e71a676eb1
ali haider <alihaidersinfo@gmail.com>
2026-04-05 02:23:36 +0500

    Setup React Router with all main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './index.css'
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Donations from "./pages/Donations";
+import Beneficiaries from "./pages/Beneficiaries";
+import Events from "./pages/Events";
+import Search from "./pages/Search";
+import Settings from "./pages/Settings";
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>helo</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/donations" element={<Donations />} />
+        <Route path="/beneficiaries" element={<Beneficiaries />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;git add .
